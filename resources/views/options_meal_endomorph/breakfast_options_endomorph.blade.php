d86141671de1277c7809498fff130980ad9c9373
bnavir <158189217+bnavir@users.noreply.github.com>
2024-08-25 21:47:04 -0400

    Update breakfast_options_endomorph.blade.php

--- resources/views/options_meal_endomorph/breakfast_options_endomorph.blade.php
+++ resources/views/options_meal_endomorph/breakfast_options_endomorph.blade.php
@@ -1,67 +1,242 @@
 @extends('layouts.layout')
 
-@section('title', 'Opciones de desayuno para ectomorfo')
+@section('title', 'Opciones de desayuno para endomorfo')
 
 @section('content')
     <div class="intro-section">
-        <h1>Más Opciones para desayuno - Ectomorfo</h1>
-        <p>Explora diferentes opciones de desayuno para variar tu dieta como ectomorfo.</p>
+        <h1>Más Opciones para desayuno - Endomorfo</h1>
+        <p>Explora diferentes opciones de desayuno para variar tu dieta como endomorfo.</p>
     </div>
     
     <div class="meal-section">
-        <h2>Opción 1: panqueques de avena</h2>
-        <ul>
-            <li>2 tazas de avena</li>
-            <li>1 taza de leche</li>
-            <li>2 huevos</li>
-            <li>1 plátano</li>
-            <li>Mezcla y cocina en una sartén antiadherente.</li>
-        </ul>
-
-        <h2>Opción 2: tostadas con aguacate</h2>
+        <h2>Opción 1: Tostadas con Mantequilla de Almendra</h2>
         <ul>
             <li>2 rebanadas de pan integral</li>
-            <li>1 aguacate maduro</li>
-            <li>Sal, pimienta, y limón al gusto</li>
+            <li>2 cucharadas de mantequilla de almendra</li>
+            <li>Puede añadir rodajas de plátano, manzana, frutos rojos y frutos secos encima</li>
         </ul>
-
-        <h2>Opción 3: Smoothie de frutas y proteínas</h2>
+        <h2>Opción 2: Tostadas con Mantequilla de Maní</h2>
+        <ul>
+            <li>2 rebanadas de pan integral</li>
+            <li>2 cucharadas de mantequilla de maní</li>
+            <li>Puede añadir rodajas de la fruta de su preferencia encima.</li>
+        </ul>
+        <h2>Opción 3: Tostadas o Pitas con Hummus</h2>
+        <ul>
+            <li>2 rebanadas de pan integral o 2 pitas</li>
+            <li>2 cucharadas de hummus</li>
+            <li>Puede añadir rodajas de pepino o zanahoria rallada encima.</li>
+        </ul>
+        <h2>Opción 4: Tostadas de Pan Integral con Salmón </h2>
+        <ul>
+            <li>2 rebanadas de pan integral</li>
+            <li>1/4 taza de salmón ahumado</li>
+            <li>1 aguacate</li>
+            <li>Sal, pimienta o limón al gusto.</li>
+        </ul>
+        <h2>Opción 5: Tostadas de Pan Integral con Queso Cottage</h2>
+        <ul>
+            <li>2 rebanadas de pan integral</li>
+            <li>1/2 taza de queso cottage</li>
+            <li>Puede añadir rodajas de tomate, miel, o lonchas de pavo encima.</li>
+        </ul>
+        <h2>Opción 6:Tostada de Pan Integral con Tomate y Jamón</h2>
+        <ul>
+            <li>1 rebanada de pan integral tostado</li>
+            <li>1 tomate frotado sobre el pan</li>
+            <li>2 lonchas de jamón serrano</li>
+            <li>1 cucharadita de aceite de oliva</li>
+        </ul>
+        <h2>Opción 7: Smoothie de Mango y Yogur</h2>
+        <ul>
+            <li>1 taza de mango (picado)</li>
+            <li>1 taza de yogur griego</li>
+            <li>1 cucharada de miel</li>
+            <li>Mezclar en la licuadora hasta obtener una textura suave.</li>
+        </ul>
+        <h2>Opción 8: Smoothie de Plátano y Avena</h2>
+        <ul>
+            <li>1 plátano</li>
+            <li>1 taza de avenas</li>
+            <li>1 taza de leche de almendras</li>
+            <li>1 cucharada de miel</li>
+            <li>Mezclar en la licuadora hasta obtener una textura suave.</li>
+        </ul>
+        <h2>Opción 9: Smoothie Verde</h2>
+        <ul>
+        <li>1 taza de leche de almendras</li>
+            <li>1 taza de espinacas</li>
+            <li>1/2 taza de piña</li>
+            <li>1/2 manzana verde</li>
+            <li>1/2 pepino</li>
+            <li>1/2 aguacate</li>
+            <li>Mezclar en la licuadora hasta obtener una textura suave.</li>
+        </ul>
+        <h2>Opción 10: Smoothie de Proteína y Fruta</h2>
         <ul>
             <li>1 taza de leche de almendras</li>
-            <li>1 scoop de proteína en polvo</li>
-            <li>1/2 taza de espinacas</li>
-            <li>1 plátano</li>
-            <li>1/2 taza de frutos rojos</li>
-            <li>Mezcla todo en la licuadora hasta que quede suave.</li>
+            <li>1-2 scoops de proteína en polvo</li>
+            <li>1/2 taza de fruta de tu preferencia</li>
+            <li>Mezclar en la licuadora hasta obtener una textura suave.</li>
         </ul>
-
-        <h2>Opción 4: Yogur griego con granola</h2>
+        <h2>Opción 11:Smoothie de Acai y Plátano</h2>
         <ul>
-            <li>1 taza de yogur griego</li>
-            <li>1/2 taza de granola</li>
-            <li>1 cucharada de miel</li>
-            <li>1/4 taza de nueces picadas</li>
-            <li>1/4 taza de pasas</li>
+            <li>1 paquete de pulpa de acai congelada</li>
+            <li>1 plátano maduro</li>
+            <li>1/2 taza de leche de almendras</li>
+            <li>1 cucharada de semillas de lino</li>
+            <li>1/4 taza de arándanos congelados</li>
         </ul>
-
-        <h2>Opción 5: Tortilla de espinacas y queso</h2>
+        <h2>Opción 12: Avena Cocida con Frutas</h2>
+        <ul>
+            <li>1 taza de avena</li>
+            <li>1 taza de leche</li>
+            <li>1/2 taza de frutas frescas</li>
+            <li>1 cucharadita de miel</li>
+            <li>Cocina y mezcla todos los ingredientes.</li>
+        </ul>
+        <h2>Opción 13: Pudding de Chía con Cacao</h2>
+        <ul>
+            <li>1/4 taza de semillas de chía</li>
+            <li>1 taza de leche de almendras</li>
+            <li>1 cucharada de cacao en polvo</li>
+            <li>1 cucharada de miel</li>
+            <li>Deja reposar en la nevera por 2 horas.</li>
+        </ul>
+        <h2>Opción 14: Magdalenas de Plátano con Avena y Frutos secos</h2>
+        <ul>
+            <li>1 taza de avena</li>
+            <li>1 plátano (machacado)</li>
+            <li>1/2 taza de frutos secos</li>
+            <li>1 huevo</li>
+            <li>Hornea a 180°C por 20 minutos.</li>
+            <li>Se puede acompañar con una taza de leche de almendras.</li>
+        </ul>
+        <h2>Opción 15: Overnight Oats con Frutos Secos</h2>
+        <ul>
+            <li>1/2 taza de avena</li>
+            <li>1/2 taza de leche de almendras</li>
+            <li>1/4 taza de frutos secos (almendras, nueces, avellanas, etc.)</li>
+            <li>1 cucharada de miel (opcional)</li>
+            <li>Frutas frescas al gusto (plátano, manzana, frutos rojos, etc.)</li>
+            <li>Mezcla todos los ingredientes en un frasco o recipiente hermético.</li>
+            <li>Deja reposar en la nevera durante toda la noche.</li>
+            <li>Al día siguiente, revuelve y disfruta frío o a temperatura ambiente.</li>
+        </ul>
+        <h2>Opción 16: Bowl de Yogurt y Frutas</h2>
+        <ul>
+            <li>1 taza de yogurt griego</li>
+            <li>1/2 taza de fruta de tu preferencia</li>
+            <li>1/4 taza de granola</li>
+            <li>1 cucharadita de miel</li>
+        </ul>
+        <h2>Opción 17: Waffles de Avena y Plátano</h2>
+        <ul>
+            <li>1 taza de avena</li>
+            <li>1 plátano (machacado)</li>
+            <li>1/2 taza de leche de almendras</li>
+            <li>1 huevo</li>
+            <li>1 cucharadita de polvo de hornear</li>
+            <li>1/2 cucharadita de canela</li>
+            <li>Mezcla todos los ingredientes hasta obtener una masa homogénea.</li>
+            <li>Vierte en una waflera precalentada y cocina hasta dorar.</li>
+            <li>Puede servir con frutas frescas o yogur griego.</li>
+        </ul>
+        <h2>Opción 18:Pancakes de Quinua</h2>
+        <ul>
+            <li>1 taza de quinua cocida y mezclada con masa de pancakes integral</li>
+            <li>1/4 taza de arándanos frescos</li>
+            <li>1 cucharada de miel o jarabe de agave</li>
+            <li>1/4 taza de yogur griego</li>
+        </ul>
+        <h2>Opción 19: Huevos revueltos con espinacas y tomates</h2>
         <ul>
             <li>3 huevos</li>
             <li>1 taza de espinacas frescas</li>
+            <li>5 tomates cherry cortados en mitad</li>
+            <li>Sal y pimienta al gusto</li>
+            <li>Prepara los huevos revueltos en una sartén antiadherente.</li>
+        </ul>
+        <h2>Opción 20: Tortilla de Verduras</h2>
+        <ul>
+            <li>2 huevos</li>
+            <li>1/2 taza de pimientos y cebolla</li>
+            <li>1 tomate en rodajas</li>
+            <li>1 taza de champiñones</li>
             <li>1/4 taza de queso rallado</li>
             <li>Sal y pimienta al gusto</li>
-            <li>Prepara la tortilla en una sartén antiadherente.</li>
+            <li>Freir la tortilla en una sartén antiadherente.</li>
         </ul>
-        
-        <h2>Opción 6: </h2>
+        <h2>Opción 21:Omelette de Espinacas y Champiñones</h2>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>3 claras de huevo y 1 huevo entero</li>
+            <li>1 taza de espinacas frescas</li>
+            <li>1/2 taza de champiñones en rodajas</li>
+            <li>1 cucharadita de aceite de oliva</li>
+            <li>Sal y pimienta al gusto</li>
         </ul>
-    </div>
+        <h2>Opción 22:Ensalada de Tomate, Pepino y Queso Feta</h2>
+        <ul>
+            <li>1 taza de tomate en cubos</li>
+            <li>1 taza de pepino en rodajas</li>
+            <li>1/4 taza de queso feta desmenuzado</li>
+            <li>1 cucharada de aceite de oliva</li>
+            <li>1 cucharadita de orégano seco</li>
+        </ul>
+        <h2>Opción 23:Paratha de Espinacas con Yogur</h2>
+        <ul>
+            <li>1 paratha de espinacas integral</li>
+            <li>1/2 taza de yogur griego natural</li>
+            <li>1 cucharadita de especias garam masala</li>
+            <li>1/4 taza de cebollino picado</li>
+        </ul>
+        <h2>Opción 24:Tostadas de Frijoles Negros y Aguacate</h2>
+        <ul>
+            <li>2 tostadas integrales</li>
+            <li>1/2 taza de frijoles negros cocidos</li>
+            <li>1/4 aguacate, en rodajas</li>
+            <li>1/4 taza de salsa fresca de tomate</li>
+            <li>1 cucharada de cilantro picado</li>
+        </ul>
+        <h2>Opción 25:Bolón de Verde con Huevo</h2>
+        <ul>
+            <li>1 bolón de verde (plátano verde machacado) hecho con un mínimo de aceite</li>
+            <li>1 huevo revuelto</li>
+            <li>1/4 taza de aguacate en rodajas</li>
+        </ul>
+        <h2>Opción 26:Empanadas de Pollo con Ensalada de Tomate</h2>
+        <ul>
+            <li>1 empanada de pollo horneada</li>
+            <li>1 taza de ensalada de tomate (tomate, cebolla, albahaca, aceite de oliva)</li>
+        </ul>
+        <h2>Opción 27:Arepa con Queso y Aguacate</h2>
+        <ul>
+            <li>1 arepa de maíz integral</li>
+            <li>1/4 taza de queso fresco</li>
+            <li>1/2 aguacate en rodajas</li>
+            <li>1 cucharadita de aceite de aguacate</li>
+        </ul>
+        <h2>Opción 28:Cachapas con Queso Fresco</h2>
+        <ul>
+            <li>1 cachapa (tortilla de maíz y elote) hecha con harina integral</li>
+            <li>1/4 taza de queso fresco</li>
+            <li>1/4 aguacate en rodajas</li>
+        </ul>
+        <h2>Opción 29:Chilaquiles con Salsa Verde</h2>
+        <ul>
+            <li>1 taza de totopos de maíz horneados</li>
+            <li>1/2 taza de salsa verde</li>
+            <li>1/4 taza de queso fresco desmenuzado</li>
+            <li>1 huevo cocido encima</li>
+        </ul>
+        <h2>Opción 30: Tortilla de Papas y Espinacas</h2>
+        <ul>
+            <li>2 papas cocidas y en rodajas</li>
+            <li>1 taza de espinacas frescas</li>
+            <li>2 huevos batidos</li>
+            <li>1 cucharadita de aceite de oliva</li>
+        </ul>
 
+    </div>
 
 @endsection
