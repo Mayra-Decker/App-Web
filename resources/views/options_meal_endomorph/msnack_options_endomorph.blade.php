7d2ac017b407ad6483a3d0b90e53ac6d8c21e7be
bnavir <158189217+bnavir@users.noreply.github.com>
2024-08-25 21:49:11 -0400

    Update msnack_options_endomorph.blade.php

---
+++ resources/views/options_meal_endomorph/msnack_options_endomorph.blade.php
@@ -0,0 +1,228 @@
+@extends('layouts.layout')
+
+@section('title', 'Opciones de snacks para endomorfo')
+
+@section('content')
+    <div class="intro-section">
+        <h1>Más Opciones para snacks - Endomorfo</h1>
+        <p>Explora diferentes opciones de snacks saludables y adecuados para tu dieta como endomorfo.</p>
+    </div>
+
+    <div class="meal-section">
+        <h2>Opción 1: Nueces y almendras</h2>
+        <ul>
+            <li>1/4 taza de nueces</li>
+            <li>1/4 taza de almendras</li>
+            <li>Combina los frutos en un bowl.</l>
+            <li>Un snack lleno de proteínas y grasas saludables.</li>
+        </ul>
+        <h2>Opción 2:Manzana con mantequilla de maní</h2>
+        <ul>
+            <li>1 manzana en rebanadas</li>
+            <li>2 cucharadas de mantequilla de maní</li>
+            <li>Untar las rebanadas de manzana con el maní</li>
+        </ul>
+        <h2>Opción 3: Plátano con Chocolate Oscuro</h2>
+        <ul>
+            <li>1 plátano maduro</li>
+            <li>1/4 taza de chocolate oscuro (70% cacao o más), derretido</li>
+           <li>Rodajas de plátano sumergidas en chocolate derretido</li>
+           <li>Colocar en una bandeja con papel pergamino y refrigerar 2 horas.</li>
+        </ul>
+        <h2>Opción 4: Mousse de Chocolate con Aguacate</h2>
+        <ul>
+            <li>1 aguacate maduro</li>
+            <li>2 cucharadas de cacao en polvo sin azúcar</li>
+            <li>2 cucharadas de miel o sirope de arce</li>
+            <li>1/2 cucharadita de extracto de vainilla</li>
+            <li>Mezcla todos los ingredientes hasta obtener una textura suave y cremosa.</li>
+        </ul>
+        <h2>Opción 5: Tazas de Chocolate con Almendras y Coco</h2>
+        <ul>
+            <li>1 taza de chocolate oscuro (70% cacao o más), derretido</li>
+            <li>1/4 taza de almendras picadas</li>
+            <li>2 cucharadas de coco rallado sin azúcar</li>
+            <li>Vierte el chocolate derretido en moldes para mini cupcakes</li> 
+            <li>Agrega almendras picadas y coco rallado en cada molde </li>
+            <li>Refrigera hasta que el chocolate se endurezca.</li>
+        </ul>
+        <h2>Opción 6: Trufas de Chocolate y Frutos Secos</h2>
+        <ul>
+            <li>1 taza de dátiles sin hueso</li>
+            <li>1/2 taza de nueces o almendras</li>
+            <li>1/4 taza de cacao en polvo</li>
+            <li>1 cucharadita de extracto de vainilla</li>
+            <li>Mezcla todos los ingredientes hasta obtener una masa pegajosa</li>
+            <li>Forma bolitas y refrigera.</li>
+        </ul>
+        <h2>Opción 7:Yogur griego con frutas</h2>
+        <ul>
+            <li>1 taza de yogur griego</li>
+            <li>1/2 taza de frutos rojos</li>
+            <li>Añadir frutos secos (almendras, nueces, etc.)</li>
+            <li>Combina todos los ingredientes en un bowl.</li>
+        </ul>
+        <h2>Opción 8: Ensalada de Frutas </h2>
+        <ul>
+            <li>1 taza de frutas mixtas de tu preferencia (kiwi, fresas, mango)</li>
+            <li>Añadir miel (opcional)</li>
+        </ul>
+        <h2>Opción 9:Gelatina sin azúcar</h2>
+        <ul>
+            <li>1 porción de gelatina sin azúcar</li>
+            <li>Prepara según las instrucciones del paquete.</li>
+        </ul>
+        <h2>Opción 10: Pudín de chía</h2>
+        <ul>
+            <li>1/4 taza de semillas de chía</li>
+            <li>1 taza de leche de almendras</li>
+            <li>1 cucharada de miel</li>
+            <li>Refrigera durante 2 horas.</li>
+        </ul>
+        <h2>Opción 11: Manzana al horno con canela</h2>
+        <ul>
+            <li>1 manzana en rodajas</li>
+            <li>1 cucharadita de canela</li>
+            <li>Hornea a 180°C durante 20 minutos.</li>
+        </ul>
+        <h2>Opción 12: Barritas de proteínas</h2>
+        <ul>
+            <li>1 taza de avena</li>
+            <li>1/2 taza de mantequilla de almendras</li>
+            <li>1/2 taza de proteína en polvo</li>
+            <li>Mezcla y refrigera hasta que se endurezca.</li>
+        </ul>
+        <h2>Opción 13: Muffins de Avena y Zanahoria</h2>
+        <ul>
+            <li>1 taza de avena integral</li>
+            <li>1/2 taza de zanahoria rallada</li>
+            <li>2 huevos</li>
+            <li>1/4 taza de puré de manzana sin azúcar</li>
+            <li>1 cucharadita de canela</li>
+            <li>1/4 taza de nueces picadas</li>
+        </ul>       
+
+        <h2>Opción 14: Muffins de Huevos y Vegetales</h2>
+        <ul>
+            <li>4 huevos</li>
+            <li>1/2 taza de espinacas picadas</li>
+            <li>1/4 taza de pimientos rojos picados</li>
+            <li>Sal y pimienta al gusto</li>
+            <li>Mezcla los ingredientes y hornea en moldes de muffin a 180°C (350°F) por 20 minutos.</li>
+        </ul>
+        <h2>Opción 15: Panqueques de Avena y Plátano</h2>
+        <ul>
+            <li>1 taza de avena</li>
+            <li>1 plátano maduro, en puré</li>
+            <li>2 huevos</li>
+            <li>1 cucharadita de polvo para hornear</li>
+            <li>Unas gotas de extracto de vainilla</li>
+        </ul>
+        <h2>Opción 16: Galletas de Avena y Plátano</h2>
+        <ul>
+            <li>1 plátano maduro</li>
+            <li>1 taza de avena</li>
+            <li>Trozos de nueces (opcional)</li>
+            <li>Mezcla, forma galletas y hornea a 180°C durante 15 minutos.</li>
+        </ul>
+        <h2>Opción 17: Bolitas de Proteína Congeladas</h2>
+        <ul>
+            <li>1 taza de proteína en polvo (sabor a tu elección)</li>
+            <li>1/2 taza de mantequilla de almendra o de maní</li>
+            <li>1/2 taza de avena</li>
+            <li>1/4 taza de chispas de chocolate oscuro (opcional)</li>
+            <li>1/4 taza de leche de almendras (o más si es necesario)</li>
+           <li>Mezcla todos los ingredientes hasta obtener una masa pegajosa.</li> 
+           <li>Forma bolitas y congela por 30 minutos</li>
+           <li>Guarda en un recipiente hermético en el congelador.</li>
+        </ul>
+        <h2>Opción 18: Chips de Kale al Horno</h2>
+        <ul>
+           <li>1 manojo de kale (col rizada)</li>
+            <li>1 cucharada de aceite de oliva</li>
+            <li>Sal al gusto</li>
+            <li>Hornea el kale con aceite de oliva y sal hasta que estén crujientes.</li>
+        </ul>
+        <h2>Opción 19:Rodajas de pepino con queso cottage</h2>
+        <ul>
+            <li>1/2 taza de queso cottage</li>
+            <li>Rodajas de pepino</li>
+            <li>Sal, limón o miel al gusto.</li>
+        </ul>
+        <h2>Opción 20: Taza de Requesón con Piña</h2>
+        <ul>
+            <li>1 taza de requesón bajo en grasa</li>
+            <li>1/2 taza de piña fresca en trozos</li>
+            <li>Mezcla el requesón con la piña para un snack dulce y proteico.</li>
+        </ul>    
+        <h2>Opción 21: Palomitas de Maíz Caseras</h2>
+        <ul>
+            <li>1/4 taza de granos de maíz para palomitas</li>
+            <li>1 cucharadita de aceite de coco</li>
+            <li>Sal al gusto</li>
+            <li>Prepara las palomitas en una olla con aceite de coco y agrega sal.</li>
+        </ul>
+        <h2>Opción 22: Palitos de Apio con Queso Cottage</h2>
+        <ul>
+            <li>Palitos de apio</li>
+            <li>2 cucharadas de queso cottage</li>
+        </ul>
+        <h2>Opción 23: Hummus con palitos de zanahoria</h2>
+        <ul>
+            <li>1/2 taza de hummus</li>
+            <li>1 zanahoria cortada en palitos.</li>
+            <li>Untar los palitos de zanahoria en el hummus</li>
+        </ul>
+        <h2>Opción 24: Palitos de Apio con Mantequilla de Almendra</h2>
+        <ul>
+            <li>4 palitos de apio</li>
+            <li>2 cucharadas de mantequilla de almendra</li>
+            <li>Unta la mantequilla de almendra en los palitos de apio.</li>
+        </ul>
+        <h2>Opción 25: Edamame al Vapor</h2>
+        <ul>
+            <li>1 taza de edamame (soja joven) al vapor</li>
+            <li>Sal y pimienta al gusto</li>
+            <li>Espolvorea con sal y pimienta después de cocinar al vapor.</li>
+        </ul>
+        <h2>Opción 26: Rollos de Pavo y Queso</h2>
+        <ul>
+            <li>3-4 lonchas de pavo</li>
+            <li>3-4 lonchas de queso bajo en grasa</li>
+            <li>Enrolla el queso con el pavo para formar rollos.</li>
+        </ul>
+        <h2>Opción 27: Rollos de Jamón y Queso</h2>
+        <ul>
+            <li>4 lonchas de jamón de pavo</li>
+            <li>4 lonchas de queso bajo en grasa</li>
+            <li>Enrolla el queso con el jamón para formar rollos.</li>
+        </ul>
+        <h2>Opción 28: Pinchos de Pollo con Vegetales</h2>
+        <ul>
+            <li>150 g de pechuga de pollo, en cubos</li>
+            <li>1 pimiento rojo, en trozos</li>
+            <li>1 calabacín, en rodajas</li>
+            <li>1 cebolla morada, en trozos</li>
+           <li>Marinar el pollo con aceite de oliva, ajo en polvo y hierbas secas</li>
+           <li>Ensartar el pollo y los vegetales en brochetas y ásalos a la parrilla.</li>
+        </ul>
+        <h2>Opción 29: Pinchos de Camarones con Piña y Pimiento</h2>
+        <ul>
+            <li>150 g de camarones, pelados y desvenados</li>
+            <li>1 taza de piña, en cubos</li>
+            <li>1 pimiento verde, en trozos</li>
+            <li>1 cucharada de salsa de soja</li>
+            <li>Ensarta los camarones, la piña y el pimiento en brochetas</li> 
+            <li>Marinar en salsa de soja y ásalos a la parrilla hasta que estén cocidos.</li>
+        </ul>
+        <h2>Opción 30: Pinchos de Tofu con Vegetales</h2>
+        <ul>
+            <li>150 g de tofu firme, en cubos</li>
+            <li>1 pimiento amarillo, en trozos</li>
+           <li>1 champiñón grande, en mitades</li>
+            <li>1 calabacín, en rodajas</li>
+            <li>Marinar el tofu con salsa de soja, aceite de sésamo y jengibre</li> 
+            <li>Ensarta el tofu y los vegetales en brochetas y cocínalos a la parrilla.</li>
+        </ul>
+    </div>
+@endsection
